c5a9a694e18cf060e7d42b42832071ad47cb36b7
MillarayM <millaray.sw@gmail.com>
2021-03-07 22:45:29 -0300

    portafolio version 2

---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+
+$("#titulotrabajos1").click(function(){
+    $("#titulotrabajos1").addClass('letra')    ;
+  })
+
+
+  $("#titulotrabajos2").click(function(){
+    $("#titulotrabajos2").addClass('letra')    ;
+  })
+
+  $("#titulotrabajos3").click(function(){
+    $("#titulotrabajos3").addClass('letra')    ;
+  })
+
+  $("#titulotrabajos4").click(function(){
+    $("#titulotrabajos4").addClass('letra')    ;
+  })
+
+  $("#titulotrabajos5").click(function(){
+    $("#titulotrabajos5").addClass('letra')    ;
+  })
+
+  $("#titulotrabajos6").click(function(){
+    $("#titulotrabajos6").addClass('letra')    ;
+  })
+
+
+  $("#tarjeta1").click(function(){
+
+    $("#contenido1").toggle('slow')    ;
+  
+  })
+
+
+  $("#tarjeta2").click(function(){
+
+    $("#contenido2").toggle('slow')    ;
+  
+  })
+
+
+  $("#tarjeta3").click(function(){
+
+    $("#contenido3").toggle('slow')    ;
+  
+  })
+
+
+
+
